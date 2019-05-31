2bcb7b3e1e7f3fcacba4c351eeb00560f4595998
fhfuih <sam.zyhuang@outlook.com>
2019-05-31 13:00:49 +0800

    Add CORS

--- server/server/json_rectifier.py
+++ server/server/json_rectifier.py
@@ -1,11 +1,25 @@
 from django.http import JsonResponse
 
+from . import settings_local
+
+def add_cors_headers(func):
+    def inner(*args, **kwargs):
+        r = func(*args, **kwargs)
+        for k, v in settings_local.cors_headers.items():
+            r[k] = v
+        for k, v in r.items():
+            print(k, ':', v)
+        return r
+    return inner
+
+@add_cors_headers
 def get_error_json(err_msg):
     ret = dict()
     ret['error'] = True
     ret['errMsg'] = err_msg
     return JsonResponse(ret, status = 400)
 
+@add_cors_headers
 def get_correct_json(data):
     ret = dict()
     ret['error'] = False
